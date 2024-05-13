f3098208414c870f4aeba1dd3cf4cbe42a81dff6
Randall White <randall.white@login-02.clusternet>
2024-05-13 11:42:52 -0700

    added the entrypoint files, and scripts for running cryosparc

---
+++ cryosparc.sh
@@ -0,0 +1,115 @@
+#!/bin/bash -x
+
+export PATH=${CRYOSPARC_MASTER_DIR}/bin:${CRYOSPARC_WORKER_DIR}/bin:${CRYOSPARC_MASTER_DIR}/deps/anaconda/bin/:$PATH
+export HOME=${HOME:-$USER_HOMEDIR}
+
+###
+# master initiation
+###
+export CRYOSPARC_MASTER_HOSTNAME=${CRYOSPARC_MASTER_HOSTNAME:-localhost}
+if [ "${CRYOSPARC_LICENSE_ID}" == "" ]; then
+  echo "CRYOSPARC_LICENSE_ID required to continue..."
+  exit 127
+fi
+# deal with multiple licenses
+if [ -z "${CRYOSPARC_LICENSE_ID##*,*}" ]; then
+  IFS=',' read -r -a licenses <<< "$CRYOSPARC_LICENSE_ID"
+  for index in "${!licenses[@]}"
+  do
+    echo "$index ${licenses[index]}"
+  done
+  CRYOSPARC_LICENSE_ID=${licenses[${HOSTNAME##*-}]}
+fi
+
+CRYOSPARC_BASE_PORT=${CRYOSPARC_BASE_PORT:-"39000"}
+
+echo "Starting cryosparc master..."
+cd ${CRYOSPARC_MASTER_DIR}
+# modify configuration
+printf "%s\n" "1,\$s/^export CRYOSPARC_MASTER_HOSTNAME=.*$/export CRYOSPARC_MASTER_HOSTNAME=${CRYOSPARC_MASTER_HOSTNAME}/g" wq | ed -s ${CRYOSPARC_MASTER_DIR}/config.sh
+printf "%s\n" "1,\$s/^export CRYOSPARC_LICENSE_ID=.*$/export CRYOSPARC_LICENSE_ID=${CRYOSPARC_LICENSE_ID}/g" wq | ed -s ${CRYOSPARC_MASTER_DIR}/config.sh
+printf "%s\n" "1,\$s|^export CRYOSPARC_DB_PATH=.*$|export CRYOSPARC_DB_PATH=${CRYOSPARC_DATADIR}/cryosparc_database|g" wq | ed -s ${CRYOSPARC_MASTER_DIR}/config.sh
+printf "%s\n" "1,\$s/^export CRYOSPARC_BASE_PORT=.*$/export CRYOSPARC_BASE_PORT=${CRYOSPARC_BASE_PORT}/g" wq | ed -s ${CRYOSPARC_MASTER_DIR}/config.sh
+if ! grep -q 'CRYOSPARC_FORCE_HOSTNAME=true' ${CRYOSPARC_MASTER_DIR}/config.sh; then
+  echo 'export CRYOSPARC_FORCE_HOSTNAME=true' >> ${CRYOSPARC_MASTER_DIR}/config.sh
+fi
+echo '====='
+cat ${CRYOSPARC_MASTER_DIR}/config.sh
+echo '====='
+
+# modify mongo path
+#sed -i 's|MONGO_URL="mongodb://%(ENV_CRYOSPARC_MASTER_HOSTNAME)s:%(ENV_CRYOSPARC_MONGO_PORT)s|MONGO_URL=mongodb://cryosparc-fpoitevi:%(ENV_CRYOSPARC_MONGO_PORT)s|g' ${CRYOSPARC_MASTER_DIR}/supervisord.conf
+#sed -i 's|MONGO_OPLOG_URL="mongodb://%(ENV_CRYOSPARC_MASTER_HOSTNAME)s:%(ENV_CRYOSPARC_MONGO_PORT)s|MONGO_OPLOG_URL="mongodb://cryosparc-fpoitevi:%(ENV_CRYOSPARC_MONGO_PORT)s|g' ${CRYOSPARC_MASTER_DIR}/supervisord.conf
+#sed -i 's|ROOT_URL="http://%(ENV_CRYOSPARC_MASTER_HOSTNAME)s:|ROOT_URL="http://cryosparc-fpoitevi:|g' ${CRYOSPARC_MASTER_DIR}/supervisord.conf
+
+# envs
+THIS_USER=$(whoami)
+THIS_USER_SUFFIX=${USER_SUFFIX:-'czbiohub.org'}
+ACCOUNT="${THIS_USER}@${THIS_USER_SUFFIX}"
+echo "Starting cryoSPARC in ${CRYOSPARC_MASTER_DIR} as ${THIS_USER}${THIS_USER_SUFFIx} with..."
+SOCK_FILE=$(cryosparcm env | grep CRYOSPARC_SUPERVISOR_SOCK_FILE | sed 's/^.*CRYOSPARC_SUPERVISOR_SOCK_FILE=//' | sed 's/"//g')
+rm -f "${SOCK_FILE}" || true
+cryosparcm restart
+
+# ensure that the mongo replset is correct
+MONGO_PORT=$(( $CRYOSPARC_BASE_PORT + 1 ))
+mongo localhost:$MONGO_PORT  <<EOF
+cfg = rs.conf()
+cfg.members[0].host = "localhost:$MONGO_PORT"
+rs.reconfig(cfg, { force: true } )
+EOF
+
+# creat cryosparc local accounts
+create_account() {
+  local account=$1;
+  local password=$2;
+  local name=$3;
+  cryosparcm createuser    --email ${account} --password ${password} --username ${name} --firstname ${name} --lastname ${name};
+  cryosparcm resetpassword --email ${account} --password ${password};
+}
+export -f create_account
+# always set the passwrod to license
+create_account ${ACCOUNT} "${CRYOSPARC_PASSWORD}" "${THIS_USER}"
+# add additional
+#if [ -e "/init.d/accounts" ]; then
+#  cat /init.d/accounts | xargs -n3 bash -c 'create_account "$0" "$1" "$2"'
+#fi
+
+# need to restart to get login prompt
+cryosparcm restart
+
+echo "Success starting cryosparc master!"
+
+# remove all existing worker threads
+${CRYOSPARC_MASTER_DIR}/bin/cryosparcm cli 'get_scheduler_targets()'  | python -c "import sys, ast, json; print( json.dumps(ast.literal_eval(sys.stdin.readline())) )" | jq '.[].name' | sed 's:"::g' | xargs -n1 -I \{\} ${CRYOSPARC_MASTER_DIR}/bin/cryosparcm cli 'remove_scheduler_target_node("'{}'")'
+
+# add additional job lanes
+if [ "${CRYOSPACE_ADD_JOB_LANES}" == "1" ]; then
+  echo "Registering job lanes..."
+  for i in `ls -1 /app/slurm/`; do
+    cd /app/slurm/$i
+    ${CRYOSPARC_MASTER_DIR}/bin/cryosparcm cluster connect
+  done
+  cd ${CRYOSPARC_MASTER_DIR}
+fi
+
+# local worker
+if [ "${CRYOSPARC_LOCAL_WORKER}" == "1" ]; then
+  echo "Starting cryosparc local worker for ${CRYOSPARC_MASTER_HOSTNAME}..."
+  export CRYOSPARC_CACHE_DIR=${CRYOSPARC_CACHE_DIR:-"/tmp/cryosparc"}
+  mkdir -p ${CRYOSPARC_CACHE_DIR}
+  NOGPU=""
+  if [ ! -z $CRYOSPARC_WORKER_NOGPU ]; then
+    NOGPU="--nogpu"
+  fi
+  SSD_OPTS="--ssdpath ${CRYOSPARC_CACHE_DIR}"
+  if [ ! -z $CRYOSPARC_WORKER_NOSSD ]; then
+    SSD_OPTS="--nossd"
+  fi
+  #  if slurm cpu var set, set cpus, otherwise yolo
+  if [ -z ${SLURM_NPROCS+x} ]; then
+    ${CRYOSPARC_WORKER_DIR}/bin/cryosparcw connect --worker ${CRYOSPARC_MASTER_HOSTNAME} --master ${CRYOSPARC_MASTER_HOSTNAME} --port ${CRYOSPARC_BASE_PORT} ${SSD_OPTS} ${NOGPU}
+  else
+    ${CRYOSPARC_WORKER_DIR}/bin/cryosparcw connect --cpus $SLURM_NPROCS --worker ${CRYOSPARC_MASTER_HOSTNAME} --master ${CRYOSPARC_MASTER_HOSTNAME} --port ${CRYOSPARC_BASE_PORT} ${SSD_OPTS} ${NOGPU}
+  fi
+fi
---
+++ entrypoint.bash
@@ -0,0 +1,189 @@
+#!/bin/bash -x
+
+function setup_user() {
+    id -u ${U_NAME} 2> /dev/null 1>&2
+    if [ $? -ne 0 ]; then
+      make_user
+    fi
+}
+
+function make_user() {
+    # If EXTERNAL_UID is not set, we just use the standard system generated
+    #  UID.
+    # If we can get a good value, the UID and GID will both be that.
+    # Reject implausibly small values.  Probably means we didn't get an
+    #  ID and so we get the (small) serial assigned by KubeSpawner
+    local nuid=""
+    if [ "${EXTERNAL_UID}" -lt 100 ]; then
+      EXTERNAL_UID=""
+    fi
+    if [ -n "${EXTERNAL_UID}" ]; then
+      nuid=" ${EXTERNAL_UID}"
+    fi
+    add_groups
+    local gentry=""
+    local suppgrp=()
+    local gid=""
+    if [ -n "${EXTERNAL_GROUPS}" ]; then
+      for gentry in $(echo ${EXTERNAL_GROUPS} | tr "," "\n"); do
+        gname=$(echo ${gentry} | cut -d ':' -f 1)
+        if [ -z "${gname}" ]; then
+          continue
+        fi
+        local group_id=$(echo ${gentry} | cut -d ':' -f 1)
+        if [ -z "${gid}" ]; then
+            gid="${group_id}"
+        fi
+        supgrp+=("$gname")
+      done
+    fi
+    makedir=0
+    if [[ ! -d "${USER_HOMEDIR}" ]]; then
+        echo "Creating homedir ${USER_HOMEDIR}..."
+        echo mkdir -p ${USER_HOMEDIR} 
+        mkdir -p ${USER_HOMEDIR} 
+        makedir=1
+    fi
+    echo adduser ${U_NAME} --disabled-password --home ${USER_HOMEDIR} -N --ingroup ${gid} --uid ${nuid} \
+       --shell ${DEFAULT_SHELL} --gecos "User"
+    adduser ${U_NAME} --disabled-password --home ${USER_HOMEDIR} -N --ingroup ${gid} --uid ${nuid} \
+       --shell ${DEFAULT_SHELL} --gecos "User"
+    for g in "${supgrp[@]}"; do
+        echo adduser ${U_NAME} $g
+        adduser ${U_NAME} $g
+    done
+    if [ "$makedir" -eq 1 ]; then 
+        echo chown ${U_NAME}:$(id -gn ${U_NAME}) ${USER_HOMEDIR}
+        chown ${U_NAME}:$(id -gn ${U_NAME}) ${USER_HOMEDIR}
+    fi
+}
+
+function add_groups() {
+    local gentry=""
+    local gname=""
+    local gid=""
+    if [ -n "${EXTERNAL_GROUPS}" ]; then
+        for gentry in $(echo ${EXTERNAL_GROUPS} | tr "," "\n"); do
+            gname=$(echo ${gentry} | cut -d ':' -f 1)
+            gid=$(echo ${gentry} | cut -d ':' -f 2)
+            add_group ${gname} ${gid}
+        done
+    fi
+}
+
+function add_group() {
+    # If the group exists already, use that.
+    # If it doesn't exist but the group id is in use, use a system-
+    #  assigned gid.
+    # Otherwise, use the group id to create the group.
+    local gname=$1
+    local gid=$2
+    local exgrp=$(getent group ${gname})
+    if [ -n "${exgrp}" ]; then
+        return
+    fi
+    if [ -n "${gid}" ]; then
+        local exgid=$(getent group ${gid})
+        if [ -n "${exgid}" ]; then
+            gid=""
+        fi
+    fi
+    local gopt=""
+    if [ -n "${gid}" ]; then
+        gopt="-g ${gid}"
+    fi
+    echo groupadd ${gopt} ${gname}
+    groupadd ${gopt} ${gname}
+}
+
+function forget_extraneous_vars() {
+    local purge="GITHUB_ACCESS_TOKEN MEM_LIMIT CPU_LIMIT"
+    unset ${purge}
+    purge_docker_vars KUBERNETES HTTPS:443
+    purge_docker_vars K8S_JLD_NGINX HTTP:80,HTTPS:443
+    purge_docker_vars JLD_FILESERVER RPCBIND:111,NFS:2049,MOUNTD:20048
+}
+
+function purge_docker_vars() {
+    local n=$1
+    local plist=$2
+    local purge="${n}_PORT"
+    local portmap=""
+    local portname=""
+    local portnum=""
+    local i=""
+    local k=""
+    for i in "HOST" "PORT"; do
+	purge="${purge} ${n}_SERVICE_${i}"
+    done
+    for portmap in $(echo ${plist} | tr "," "\n"); do
+        portname=$(echo ${portmap} | cut -d ':' -f 1)
+	purge="${purge} ${n}_SERVICE_PORT_${portname}"
+        portnum=$(echo ${portmap} | cut -d ':' -f 2)
+	for prot in "TCP" "UDP"; do
+	    k="${n}_PORT_${portnum}_${prot}"
+	    purge="${purge} ${k}"
+	    for i in "ADDR" "PORT" "PROTO"; do
+		purge="${purge} ${k}_${i}"
+	    done
+	done
+    done
+    unset ${purge}
+}
+
+
+U_NAME="${USERNAME}"
+HOMEDIRS=${HOMEDIRS:="/home"}
+export USER_HOMEDIR="${HOMEDIRS}/${U_NAME}"
+DEFAULT_SHELL="/bin/bash"
+
+sudo=""
+if [ $(id -u) -eq 0 ]; then
+    if [ -n "${U_NAME}" ]; then
+        setup_user
+        sudo="sudo -E -u ${U_NAME} "
+    else
+        echo 1>&2 "Warning: running as UID 0"
+    fi
+fi
+
+
+# ensure we have a cryosparc directory under home
+#export CRYOSPARC_DATADIR=${USER_HOMEDIR}/cryosparc-v2
+echo "Creating cryosparc datadir ${CRYOSPARC_DATADIR}..."
+mkdir -p ${CRYOSPARC_DATADIR} 
+mkdir -p ${CRYOSPARC_DATADIR}/run
+mkdir -p ${CRYOSPARC_DATADIR}/cryosparc2_database
+
+#if [[ ! -e "${CRYOSPARC_DATADIR}/config.sh" ]]; then
+#    # copy config
+    cp ${CRYOSPARC_MASTER_DIR}/config.sh ${CRYOSPARC_DATADIR}/config.sh
+#fi
+
+#if [[ ! -e "${CRYOSPARC_DATADIR}/worker-config.sh" ]]; then
+    # copy
+#    cp ${CRYOSPARC_WORKER_DIR}/config.sh ${CRYOSPARC_DATADIR}/worker-config.sh
+#fi
+ 
+#chown -R ${U_NAME} ${CRYOSPARC_DATADIR}
+
+#ln -sf ${CRYOSPARC_DATADIR}/config.sh ${CRYOSPARC_MASTER_DIR}/config.sh
+#ln -sf ${CRYOSPARC_DATADIR}/worker-config.sh ${CRYOSPARC_WORKER_DIR}/worker-config.sh
+#ln -sf ${CRYOSPARC_DATADIR}/run ${CRYOSPARC_MASTER_DIR}/run
+
+# stupid thing wants to create temp files within the master dir
+#chown ${U_NAME} ${CRYOSPARC_MASTER_DIR}/
+#chown ${U_NAME} ${CRYOSPARC_WORKER_DIR}/
+
+#cat ${CRYOSPARC_MASTER_DIR}/config.sh
+#ls -lah ${CRYOSPARC_MASTER_DIR}
+
+# change code
+#sed -i 's:    disk_has_space=.*:    disk_has_space="true":g' /app/cryosparc2_master/bin/cryosparcm
+
+# gui changes
+#sed -i 's|"url": "/fonts/woff|"url": "/user/ytl/proxy/absolute/39000/fonts/woff/|g' /app/cryosparc2_master/cryosparc2_webapp/bundle/programs/web.browser/program.json
+#chmod -R ugo+rwx /app/cryosparc2_master/cryosparc2_webapp/bundle/
+
+# down privs to user
+exec  /cryosparc.sh
